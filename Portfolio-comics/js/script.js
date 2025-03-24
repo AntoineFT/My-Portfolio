4068cd8dd82626f38e8a3778cecba3ff77999398
AntoineFT <antoine.fournierT@gmail.com>
2025-03-24 16:05:51 +0100

    Delete Portfolio-comics/js/script.js

--- Portfolio-comics/js/script.js
+++
@@ -1,9 +0,0 @@
-function openBook() {
-    const cover = document.querySelector('.cover');
-    const inside = document.querySelector('.inside');
-    gsap.to(cover, { rotationY: -180, duration: 1, ease: "power2.inOut" });
-    setTimeout(() => {
-        cover.classList.add('open');
-        inside.classList.add('visible');
-    }, 500);
-}
